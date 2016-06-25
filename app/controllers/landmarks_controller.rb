a9a81c6cde35bcf1753a6f33672576aff48bf607
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-25 19:07:49 +0000

    done

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
-
+    get '/landmarks' do
+       @landmarks = Landmark.all
+       erb :'/landmarks/index'
+    end
+    
+    get '/landmarks/new' do
+        erb :'/landmarks/new'
+    end
+    
+    post '/landmarks' do
+        @landmark = Landmark.create(params[:landmark])
+        redirect "/landmarks/#{@landmark.id}"
+    end
+    
+    get '/landmarks/:id' do
+       @landmark = Landmark.find(params[:id])
+       erb :'/landmarks/show'
+    end
+    
+    get '/landmarks/:id/edit' do
+        @landmark = Landmark.find(params[:id])
+        erb :'/landmarks/edit' 
+    end
+    
+    patch '/landmarks/:id' do
+       @landmark = Landmark.find(params[:id]) 
+       @landmark.update(params[:landmark])
+       redirect "/landmarks/#{@landmark.id}"
+    end
 end
